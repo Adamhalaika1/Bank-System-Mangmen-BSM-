1a2a21d85252f6b8de3ca54ac92d05ecbbf5c516
Adamhalaika1 <adamhalaik@gmail.com>
2024-02-18 18:15:04 +0200

    Adjustment to classes and improvements

---
+++ BankSystem1/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem1
+{
+    public class User
+    {
+        public int Id { get; }
+        public string Email { get; }
+        public string Name { get; }
+        public int Age { get; }
+        public decimal Balance { get; }
+        public string Status { get; }
+        public enum UserTypes { Admin, User }
+        public UserTypes UserType { get; set; }
+        public User(int id, string email, string name, int age, decimal balance, string status, UserTypes userType)
+        {
+            Id = id;
+            Email = email;
+            Name = name;
+            Age = age;
+            Balance = balance;
+            Status = status;
+            UserType = userType;
+        }
+
+
+
+        public virtual void PrintUserType()
+        {
+            Console.WriteLine("Login successful as user!");
+        }
+
+        public bool IsAdmin()
+        {
+            return true;
+        }
+
+        public bool IsUser()
+        {
+            return false;
+        }
+    }
+
+}
